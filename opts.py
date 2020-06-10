950c2fd52ac7a558f73838665450840e7a689f94
Ted Yap <tyap@iwu.edu>
2020-06-10 10:46:44 -0500

    set project arguments

---
+++ opts.py
@@ -0,0 +1,16 @@
+import argparse
+
+
+def configure_args():
+    parser = argparse.ArgumentParser(description='Skin Condition Tracking Model')
+
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
+    parser.add_argument('--frame_size', type=int, default=30, help='Maximum number of frames per user')
+    parser.add_argument('--image_size', type=int, default=160, help='Image width and height')
+    parser.add_argument('--epoch', type=int, default=10, help='Epoch number')
+    parser.add_argument('--data_size', type=int, default=50, help='Dataset size')
+
+    parser.add_argument('--start', type=int, default=0, help='Dataset size')
+    parser.add_argument('--end', type=int, default=0, help='Dataset size')
+
+    return parser.parse_args()
