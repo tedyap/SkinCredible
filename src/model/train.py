4f69cb7d48e55137fd4deeb76d57c732080964a0
Ted Yap <tyap@iwu.edu>
2020-06-12 13:53:52 -0500

    added parameters

--- src/model/train.py
+++ src/model/train.py
@@ -83,12 +83,12 @@ if __name__ == "__main__":
 
     shapes = (({'img': [args.frame_size, args.image_size, args.image_size, 3], 'mask': [args.frame_size]}), [None])
 
-    train_dataset = tf.data.Dataset.from_generator(lambda: data_generation(partition['train'][:args.data_size]),
+    train_dataset = tf.data.Dataset.from_generator(lambda: data_generation(partition['train'][:args.data_size], label, args),
                                                    output_types=types, output_shapes=shapes).batch(BATCH_SIZE)
     validation_dataset = tf.data.Dataset.from_generator(
-        lambda: data_generation(partition['validation'][:args.data_size]),
+        lambda: data_generation(partition['validation'][:args.data_size], label, args),
         output_types=types, output_shapes=shapes).batch(BATCH_SIZE)
-    test_dataset = tf.data.Dataset.from_generator(lambda: data_generation(partition['test'][:args.data_size]),
+    test_dataset = tf.data.Dataset.from_generator(lambda: data_generation(partition['test'][:args.data_size], label, args),
                                                   output_types=types, output_shapes=shapes).batch(BATCH_SIZE)
 
     logging.info('Initializing model...')
