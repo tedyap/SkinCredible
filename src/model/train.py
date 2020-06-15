116456eb0e03e0e06a6a920d40a73f9c4873016a
Ted Yap <tyap@iwu.edu>
2020-06-14 22:14:34 -0500

    add another input mask

--- src/model/train.py
+++ src/model/train.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
 
         max_1 = MaxPool3D(pool_size=(1, 2, 2), padding='same')(batch_1)
 
-        conv_2 = ConvLSTM2D(filters=10, kernel_size=(3, 3), padding='same', return_sequences=True)(max_1)
+        conv_2 = ConvLSTM2D(filters=10, kernel_size=(3, 3), padding='same')(max_1, mask=input_mask)
 
         batch_2 = BatchNormalization()(conv_2)
 
