5b73eae690f4c390708f6af0834c4fb97d4bbcea
Ted Yap <tyap@iwu.edu>
2020-06-14 22:28:33 -0500

    rename input

--- src/model/train.py
+++ src/model/train.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
 
         batch_1 = BatchNormalization()(conv_1)
 
-        conv_2 = ConvLSTM2D(filters=10, kernel_size=(3, 3), padding='same')(max_1, mask=input_mask)
+        conv_2 = ConvLSTM2D(filters=10, kernel_size=(3, 3), padding='same')(batch_1, mask=input_mask)
 
         batch_2 = BatchNormalization()(conv_2)
 
