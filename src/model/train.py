a8ae41bb99e9afd131f740908f6523b47483ca14
Ted Yap <tyap@iwu.edu>
2020-06-14 23:33:25 -0500

    convert user id to str

--- src/model/train.py
+++ src/model/train.py
@@ -25,6 +25,7 @@ def data_generation(user_id_list_temp, label, args):
     # Generate data
     for i, user_id in enumerate(user_id_list_temp):
         # rename error
+        user_id = str(user_id)
         if user_id[:3] == '999' and not (user_id == '9993' or user_id == '9996'):
             user_id = '#' + user_id[3:]
 
